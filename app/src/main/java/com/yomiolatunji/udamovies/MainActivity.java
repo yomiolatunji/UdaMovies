64e9a639b080ed4760fe41d18956447a87141e2b
Oluwayomi <yomiiolatunji@gmail.com>
2017-04-16 05:44:37 +0100

    Sort Added

--- app/src/main/java/com/yomiolatunji/udamovies/MainActivity.java
+++ app/src/main/java/com/yomiolatunji/udamovies/MainActivity.java
@@ -5,12 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
 
 import java.util.List;
 
@@ -21,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     private ImageView noConnectionView;
     private MoviesAdapter adapter;
     private GridLayoutManager layoutManager;
+    private FloatingActionMenu menuView;
+    private FloatingActionButton menuItemPopular;
+    private FloatingActionButton menuItemRating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +35,17 @@ public class MainActivity extends AppCompatActivity {
         moviesRecyclerView = (RecyclerView) findViewById(R.id.movies_grid);
         loadingBar = (ProgressBar) findViewById(R.id.loading);
         noConnectionView = (ImageView) findViewById(R.id.no_connection);
+        menuView = (FloatingActionMenu) findViewById(R.id.menu);
+        menuItemPopular = (FloatingActionButton) findViewById(R.id.menu_item_popular);
+        menuItemRating = (FloatingActionButton) findViewById(R.id.menu_item_rating);
 
         adapter = new MoviesAdapter(MainActivity.this);
-        MovieApiNetworkService movieApiNetworkService = MovieApiNetworkService.newInstance(MainActivity.this);
+        final MovieApiNetworkService movieApiNetworkService = MovieApiNetworkService.newInstance(MainActivity.this);
         if (NetworkUtils.isNetworkAvailable(MainActivity.this)) {
-            getData(movieApiNetworkService,getString(R.string.sort_popular));
+            getData(movieApiNetworkService, getString(R.string.sort_popular));
         } else {
             moviesRecyclerView.setVisibility(View.GONE);
+            menuView.setVisibility(View.GONE);
             loadingBar.setVisibility(View.GONE);
             noConnectionView.setVisibility(View.VISIBLE);
         }
@@ -45,18 +53,33 @@ public class MainActivity extends AppCompatActivity {
         layoutManager = new GridLayoutManager(this, 2);
         moviesRecyclerView.setLayoutManager(layoutManager);
         moviesRecyclerView.setHasFixedSize(true);
+        menuItemPopular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getData(movieApiNetworkService, getString(R.string.sort_popular));
+            }
+        });
+        menuItemRating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getData(movieApiNetworkService, getString(R.string.sort_top_rated));
+            }
+        });
     }
 
-    private void getData(MovieApiNetworkService movieApiNetworkService,String sortBy) {
+    private void getData(MovieApiNetworkService movieApiNetworkService, String sortBy) {
         loadingBar.setVisibility(View.VISIBLE);
+        menuView.setVisibility(View.GONE);
         moviesRecyclerView.setVisibility(View.GONE);
         noConnectionView.setVisibility(View.GONE);
+        adapter.clear();
         movieApiNetworkService.getMovieList(sortBy, new DataLoadingCallback<List<Movies>>() {
             @Override
             public void onResponse(List<Movies> data) {
                 if (data != null && data.size() > 0) {
                     adapter.addAndResort(data);
                     moviesRecyclerView.setVisibility(View.VISIBLE);
+                    menuView.setVisibility(View.VISIBLE);
                 }
                 loadingBar.setVisibility(View.GONE);
 
@@ -69,19 +92,4 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_sort) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
